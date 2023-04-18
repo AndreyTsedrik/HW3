df556a5f3f3ba5571137465a5e301ecfce7147e2
AndreyTsedrik <awesome.foxe@yandex.ru>
2023-04-18 10:15:03 +0300

    Task1 ok

---
+++ Tasks/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+string s = Console.ReadLine();
+int len = s.Length;
+
+if (len == 5)
+{
+    if (s[0] == s[4] && s[1] == s[3])
+    {
+        Console.WriteLine($"{s} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{s} - Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Введенное число не является пятизначным");
+}
